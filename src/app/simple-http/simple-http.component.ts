7dd4367b9b9b6dd4b526ea23856535198df3be7a
Lester <lester.ung@pathmazing.com>
2017-07-04 15:41:24 +0700

    make request to service with http module

--- src/app/simple-http/simple-http.component.ts
+++ src/app/simple-http/simple-http.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Http, Response } from '@angular/http';
 @Component({
   selector: 'app-simple-http',
   templateUrl: './simple-http.component.html',
   styleUrls: ['./simple-http.component.css']
 })
 export class SimpleHttpComponent implements OnInit {
+  loading: boolean;
+  data: object;
+  constructor(private http: Http) { }
 
-  constructor() { }
+  makeRequest(): void {
+    this.loading = true;
+    this.http.request('https://jsonplaceholder.typicode.com/posts/2')
+    .subscribe((res: Response) => {
+      this.data = res.json();
+      this.loading = false;
+    }, (error: any) => {
+      this.loading = false;
+    }, () => {
+      console.log('request completed');
+    });
+  }
 
   ngOnInit() {
   }
